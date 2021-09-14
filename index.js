8e26adb1f80243e597aa3a68d7f343f4e1917710
Abby Anderson <abby.e.anderson@gmail.com>
2021-09-14 17:16:51 -0600

    Working on Saved List function, some other small stuff too but I dont remember details srryyyy

--- index.js
+++ index.js
@@ -17,19 +17,48 @@ let selectActivity = (event) => {
     //rn i'm thinking that, when someone clicks 'save' on an activity, it moves it down to a selection below. once they have a list they want to save, they give it a name, and click 'submit', which will trigger the saveActivity fucntion to actually save the list to the db!
     event.preventDefault();
     const selectedActivity = event.target.parentNode;
+    selectedActivity.className = 'selected-list-element';
     console.log(event.target.parentNode);
     console.log()
     formList.append(selectedActivity);
 }
 
+
 let saveActivity = (event) => {
-        //this function saves specific selected activities to a local db list
-        event.preventDefault();
+    //this function saves specific selected activities to a local db list
+    console.log(event);
+    event.preventDefault();
+    
+    //here's where we could add the fetch POST to save these to the local db
+    const newListName = document.querySelector("input#list-name-input");
+    const newListNameValue = newListName.value;
+    console.log(newListNameValue);
 
-        //here's where we could add the fetch POST to save these to the local db
-        const newListName = document.querySelector("#list-name-input").value;
-        console.log(newListName);
+    //now, how to grab the list elements that are now in the form. maybe something about children with class = selected-list-element
+    let listArray = document.querySelectorAll('.selected-list-element');
+    console.log(typeof listArray);
+    console.log(listArray);
 
+    let listArrayTwo = document.getElementsByClassName('.selected-list-element');
+    console.log(listArrayTwo);
+
+    //#selected-activities - ul, parent node
+    //.selected-list-element could be child nodes?
+    let listArrayThree = document.getElementById('#selected-activities');
+    let childNodeList = listArrayThree.childNodes;
+    console.log(childNodeList);
+
+
+    //creating a new object to save to the local db
+    // const newListSavedObj = {
+    //     name: newListNameValue, 
+    //     activity: ,//selected list element
+    //     id: , //automatically assigned?
+    // }
+
+    //then do fetch request to post this to the local db
+    
+    
 }
 
 let deleteActivity = (event) => {
@@ -37,37 +66,38 @@ let deleteActivity = (event) => {
     console.log(event);
     console.log(event.target.parentNode);
     event.target.parentNode.remove();
-
+    
     //would like to also add a section of this regarding DELETE fetch, so that we can delete items that were saved to a local db list --maybe would be in a separate function just for separation of concerns
 }
 
 let callSavedActivity = () => {
     //this function lets you click on the title/heading/whatever of a saved list and see the items on the list
-
+    
     //this would likely be a get request to the local db, and would show them on the page
-
+    
 }
 
 let completeActivity = () => {
-    //this function will be called when you click on the done button, and it will 
+    //this function will be called when you click on the done button, and it will save to a list of completed activities on the local db
 }
 
 let renderActivity = (data) => {
     //console.log(data)
     const newActivity = data;
     console.log(newActivity);
-
+    
     const newLi = document.createElement('li');
     newLi.textContent = newActivity;
+    newLi.className = 'new-list-element';
     listContainer.appendChild(newLi);
-
+    
     //change the 'save' text to a heart
     const saveButton = document.createElement('button');
     saveButton.textContent = "save";
     saveButton.className = 'btn btn-primary'; 
     newLi.prepend(saveButton);
     saveButton.addEventListener('click', selectActivity);
-
+    
     //change the 'delete' text to an x
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'delete';
@@ -75,7 +105,7 @@ let renderActivity = (data) => {
     deleteButton.className = 'btn btn-danger';
     newLi.prepend(deleteButton);
     deleteButton.addEventListener('click', deleteActivity);
-
+    
     //going to add another button, a check mark, to check once you've completed an activity. ideally it will save all the completed activities to a db.json object
     const doneButton = document.createElement('button');
     doneButton.textContent = 'done';
@@ -83,7 +113,7 @@ let renderActivity = (data) => {
     doneButton.className = 'btn btn-success';
     newLi.prepend(doneButton);
     doneButton.addEventListener('click', completeActivity);
-
+    
 }
 
 let fetchData = (url) => {
@@ -98,18 +128,9 @@ let activityFactory = (event) => {
 }
 
 
+listName.addEventListener('submit', saveActivity);
 randomButton.addEventListener('click', activityFactory)
-
-
-let anotherFunction = () => {
-    console.log("Yoink!")
-}
-
-
-let testFunction = () => {
-    console.log("Hey guys, I'm a troll")
-}
-
+document.addEventListener('DOMContentLoaded', init);
 
 
 
@@ -124,8 +145,5 @@ let testFunction = () => {
 
 //choose from a drop down menu of types if you have an idea of what you might be interested in
 
-
-
-document.addEventListener('DOMContentLoaded', init);
 
 
